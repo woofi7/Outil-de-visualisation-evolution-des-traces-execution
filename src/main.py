cb3d01f1d3b2b8a3d2462a9cf195983500e56035
Tommy Bédard <58239919+Tommy-TI@users.noreply.github.com>
2023-05-17 18:29:07 -0400

    add comments

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from controller.HomeController import HomeController
 from model.HomeModel import HomeModel
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication(sys.argv)  # Create a QApplication instance
+
+    # Create instances of the view, model, and controller
     homePage = HomeView()
     homeModel = HomeModel()
     homeController = HomeController(homePage, homeModel)
-    sys.exit(app.exec())
 
-    
+    sys.exit(app.exec())  # Start the application event loop
 
 if __name__ == "__main__":
     main()
