d8009815b09c0270186ed2e241ad6884221f9843
EduardoLozaRojas <73665126+EduardoLozaRojas@users.noreply.github.com>
2023-05-31 12:07:56 -0400

    ajout du splitter dans l'interface

--- src/view/TraceVisualizerView.py
+++ src/view/TraceVisualizerView.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QListWidget, QHBoxLayout, QGridLayout, QTabWidget, QComboBox
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QListWidget, QHBoxLayout, QGridLayout, QTabWidget, QComboBox, QSplitter, QFrame
 from PyQt6 import QtCore,QtWidgets
 import sys
 import matplotlib
@@ -17,23 +17,37 @@ class TraceVisualizerView(QWidget):
         # self.setMaximumSize(800, 600)
 
         self_layout = QHBoxLayout(self)
-        left_layout = QVBoxLayout()
-        right_layout = QVBoxLayout()
-        self_layout.addLayout(left_layout)
-        self_layout.addLayout(right_layout)
+        # left_layout = QVBoxLayout()
+        # right_layout = QVBoxLayout()
+        # self_layout.addLayout(left_layout)
+        # self_layout.addLayout(right_layout)
 
         # Left Layout
+        left_frame = QFrame()
+        left_layout = QVBoxLayout(left_frame)
         label = QLabel("Current log instructions")  # Create a label widget
         self.commits_list = QListWidget()
         left_layout.addWidget(label)  # Add the label to the layout
         left_layout.addWidget(self.commits_list)
 
         # Right Layout
+        right_frame = QFrame()
+        right_layout = QVBoxLayout(right_frame)
         filters_label = QLabel("Filters")
+        filters_label.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Fixed)
         filters = QComboBox()
         filters.addItems(["All", "Added", "Deleted", "Modified"])
         right_layout.addWidget(filters_label)
         right_layout.addWidget(filters)
+
+        splitter_vertical = QSplitter(QtCore.Qt.Orientation.Vertical)
+        splitter_vertical.addWidget(right_frame)
+
+        splitter = QSplitter(QtCore.Qt.Orientation.Horizontal)
+        splitter.addWidget(left_frame)
+        splitter.addWidget(right_frame)
+
+        self_layout.addWidget(splitter)
 
         # Plot
         canvas, axes = self._create_plot(6, 6, 100)
