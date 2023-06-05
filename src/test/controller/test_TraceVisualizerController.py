d84b01760f5a52587bcbff8f8522288153ca383f
maximegagnon112 <gagnonmaximebasket@gmail.com>
2023-06-05 10:43:47 -0400

    fix test

--- src/test/controller/test_TraceVisualizerController.py
+++ src/test/controller/test_TraceVisualizerController.py
@@ -9,15 +9,16 @@ from PyQt6.QtWidgets import QLineEdit, QApplication
 class test_TraceVisualizerController(unittest.TestCase):
         
     
-    def test_ok_button_clicked(self):
+    def ok_button_clicked(self):
         app = QApplication([])
         view = Mock()
         model = Mock()
         home_view = Mock()
+        home_model = Mock()
         home_view.repoList.currentText = MagicMock(return_value='test')
         model.getCommitChanges = MagicMock(return_value = [['test', 'test', 'test', 'test']] )
-        tvc = TraceVisualizerController(view, model, home_view)
-        tvc.show_commit_changes(QLineEdit('test: test: test: test, File: '))
+        tvc = TraceVisualizerController(view, model, home_view, home_model)
+        tvc.show_commit_changes()
         self.assertIsNotNone(tvc.CommitWindowView)
         view.commit_windows.append.assert_called_once()
    
