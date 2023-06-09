3de989dd8f96351e0f9445132eb3352efa395aac
coutug <coutug18@hotmail.com>
2023-06-08 22:12:58 -0400

    finish algo

--- src/frameworkStrategies/StrategyLog4p.py
+++ src/frameworkStrategies/StrategyLog4p.py
@@ -1,6 +1,6 @@
-from frameworkStrategies.Strategy import Strategy
+import re
 
-class StrategyLog4p(Strategy):
+class StrategyLog4p():
     instance = None
     
     # Singleton
@@ -10,6 +10,52 @@ class StrategyLog4p(Strategy):
         return cls.instance
     
     # Function to get the logs specific to this framework
-    def getLogs(self):
-        # TODO
-        return
+    def getLogs(self, beforeCode, afterCode):
+        hasFramework = False
+        logs = {}
+        deletedLogs = []
+        untouchedLogs = []
+        modifiedLogs = []
+        addedLogs = []
+
+        # Check for log4j import in the before and after code
+        if "import " in beforeCode + afterCode and "log4p" in beforeCode + afterCode:
+            hasFramework = True
+
+        if hasFramework:
+            logPattern = r'\.(debug|info|warning|error)\((.*?)\)'
+            beforeMatches = re.findall(logPattern, beforeCode)
+            afterMatches = re.findall(logPattern, afterCode)
+
+            # Iterate over a copy of beforeMatches to avoid deleting items while iterating
+            for beforeMatch in beforeMatches[:]:
+                for afterMatch in afterMatches[:]:
+                    if beforeMatch[0] == afterMatch[0] and beforeMatch[1] == afterMatch[1]:
+                        untouchedLogs.append(beforeMatch[0] + beforeMatch[1])
+                        afterMatches.remove(afterMatch)
+                        beforeMatches.remove(beforeMatch)
+                        break
+
+            for beforeMatch in beforeMatches[:]:
+                for afterMatch in afterMatches[:]:
+                    if (beforeMatch[0] == afterMatch[0] and beforeMatch[1] != afterMatch[1]) or (beforeMatch[0] != afterMatch[0] and beforeMatch[1] == afterMatch[1]):
+                        modifiedLogs.append([beforeMatch[0] + beforeMatch[1], afterMatch[0] + afterMatch[1]])
+                        afterMatches.remove(afterMatch)
+                        beforeMatches.remove(beforeMatch)
+                        break
+
+            for beforeMatch in beforeMatches[:]:
+                if len(beforeMatches) >= len(afterMatches):
+                    deletedLogs.append(beforeMatch[0] + beforeMatch[1])
+                    beforeMatches.remove(beforeMatch)
+
+            for afterMatch in afterMatches[:]:
+                addedLogs.append(afterMatch[0] + afterMatch[1])
+                afterMatches.remove(afterMatch)
+
+            logs["deletedLogs"] = deletedLogs
+            logs["untouchedLogs"] = untouchedLogs
+            logs["modifiedLogs"] = modifiedLogs
+            logs["addedLogs"] = addedLogs
+
+            return logs
--- src/test/frameworkStrategies/test_StrategyLog4j.py
+++ src/test/frameworkStrategies/test_StrategyLog4j.py
@@ -8,6 +8,12 @@ class TestStrategyLog4j(unittest.TestCase):
     def setUp(self):
         self.strat = StrategyLog4j()
         
+    def test_logs_empty(self):
+        beforeCode = ""
+        afterCode = ""
+        logs = self.strat.getLogs(beforeCode=beforeCode, afterCode=afterCode)
+        self.assertEqual(logs, None)
+    
     def test_getLogs_untouched(self):
         beforeCode = """import log4j;
                         logger.info('test1');
@@ -22,19 +28,32 @@ class TestStrategyLog4j(unittest.TestCase):
         self.assertEqual(len(logs["modifiedLogs"]), 0)
         self.assertEqual(len(logs["addedLogs"]), 0)
         
-    def test_getLogs_deleted(self):
-        beforeCode = """import log4j;
-                        logger.info('test1');
-                        logger.info('test2');"""
-        afterCode = """import log4j;
-                        logger.info('test3');"""
+    def test_logs_modified(self):
+        beforeCode = '''import log4j;
+                        logger.info("test1");
+                        logger.info("test2");'''
+        afterCode = '''import log4j;
+                       logger.warn("test3");
+                       logger.info("test2");'''
         logs = self.strat.getLogs(beforeCode=beforeCode, afterCode=afterCode)
-        
         self.assertEqual(len(logs["deletedLogs"]), 1)
-        self.assertEqual(len(logs["untouchedLogs"]), 0)
-        self.assertEqual(len(logs["modifiedLogs"]), 1)
+        self.assertEqual(len(logs["untouchedLogs"]), 1)
+        self.assertEqual(len(logs["modifiedLogs"]), 0)
+        self.assertEqual(len(logs["addedLogs"]), 1)
+        
+    def test_logs_deleted(self):
+        beforeCode = '''import log4j;
+                        logger.info("test1");
+                        logger.info("test2");'''
+        afterCode = '''import log4j;
+                       logger.info("test1");'''
+        logs = self.strat.getLogs(beforeCode=beforeCode, afterCode=afterCode)
+        self.assertEqual(len(logs["deletedLogs"]), 1)
+        self.assertEqual(len(logs["untouchedLogs"]), 1)
+        self.assertEqual(len(logs["modifiedLogs"]), 0)
         self.assertEqual(len(logs["addedLogs"]), 0)
         
+    
         
     def test_getLogs_deleted(self):
         beforeCode = """import log4j;
@@ -50,6 +69,38 @@ class TestStrategyLog4j(unittest.TestCase):
         self.assertEqual(len(logs["untouchedLogs"]), 0)
         self.assertEqual(len(logs["modifiedLogs"]), 1)
         self.assertEqual(len(logs["addedLogs"]), 0)
+        
+    def test_complex_logs(self):
+        beforeCode = '''import log4j;
+                        logger.info("Starting processing");
+                        logger.debug("Configuration loaded successfully");
+                        for file in files:
+                            logger.info("Processing file: %s", file);
+                            logger.fatal("Processing file: %s", file);
+                            try:
+                                process_file(file);
+                                logger.info("File %s processed successfully", file);
+                            except Exception as e:
+                                logger.error("Error while processing file %s: %s", file, e);
+                        logger.info("Processing finished");'''
+        afterCode = '''import log4j;
+                       logger.info("Starting processing");
+                       logger.debug("Configuration loaded successfully");
+                       for file in files:
+                           logger.info("Processing file: %s", file);
+                           try:
+                               process_file(file);
+                               logger.info("File %s processed successfully", file);
+                           except ValueError as ve:
+                               logger.error("Error while processing file %s: %s", file, e);
+                           except Exception as e:
+                               logger.warn("Invalid file %s: %s", file, ve);
+                       logger.info("Processing finished");'''
+        logs = self.strat.getLogs(beforeCode=beforeCode, afterCode=afterCode)
+        self.assertEqual(len(logs["deletedLogs"]), 1)
+        self.assertEqual(len(logs["untouchedLogs"]), 6)
+        self.assertEqual(len(logs["modifiedLogs"]), 0)
+        self.assertEqual(len(logs["addedLogs"]), 1)
         
 if __name__ == "__main__":
     unittest.main()
