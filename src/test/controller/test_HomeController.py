eb84663795a855ded72a7ec9c6c3fd14224be4d9
maximegagnon112 <gagnonmaximebasket@gmail.com>
2023-07-15 16:02:12 -0400

    fix test

--- src/test/controller/test_HomeController.py
+++ src/test/controller/test_HomeController.py
@@ -15,14 +15,15 @@ import sys
 
 class test_HomeController(unittest.TestCase):
         
-    
-    def test_validate_date_range_range_valid(self):
+    @patch('model.ReposManager.ReposManager.get_repos')
+    @patch('view.HomeView.HomeView.setRepos')
+    def test_validate_date_range_range_valid(self, mock_repos_manager, mock_set_repos):
         app = QApplication([])
         homePage = Mock()
         homeModel =  Mock()
         homePage.repoList.currentText =  MagicMock(return_value='path')
+        mock_repos_manager.return_value = None
         homeController = HomeController()
-        homeController.repos_manager.get_repos = MagicMock(return_value=['path'])
         homeController.repos_manager.get_branches = MagicMock(return_value=['branch'])
         homeController.home_view= Mock()
         homeController.home_view.from_calendar.selectedDate = MagicMock(return_value=QDate(2023, 1, 1))
@@ -34,13 +35,15 @@ class test_HomeController(unittest.TestCase):
         homeController.home_view.from_date_label.setText.assert_called_once_with("FROM: Sun Jan 1 2023")
         homeController.home_view.to_date_label.setText.assert_called_once_with("TO: Mon Jan 2 2023")
         self.assertEqual(printed_output, "Valid date range")
-
-    def test_validate_date_range_range_invalid(self):
+    
+    @patch('model.ReposManager.ReposManager.get_repos')
+    @patch('view.HomeView.HomeView.setRepos')
+    def test_validate_date_range_range_invalid(self, mock_repos_manager, mock_set_repos):
         app = QApplication([])
         homePage = Mock()
         homeModel =  Mock()
+        mock_repos_manager.return_value = None
         homeController = HomeController()
-        homeController.repos_manager.get_repos = MagicMock(return_value=['path'])
         homeController.repos_manager.get_branches = MagicMock(return_value=['branch'])
         homeController.home_view= Mock()
         homePage.repoList.currentText =  MagicMock(return_value='path')
@@ -50,44 +53,46 @@ class test_HomeController(unittest.TestCase):
         homeController.home_view.from_date_label.setText.assert_called_once_with("FROM: Mon Jan 2 2023")
         homeController.home_view.to_date_label.setText.assert_called_once_with("TO: Sun Jan 1 2023")
         homeController.home_view.popupError.assert_called_once_with("Invalid Date Range", "La date 'FROM' ne peut pas être postérieure à la date 'TO'.")
-
-    def test_new_repo_button_clicked(self):
+    
+    @patch('model.ReposManager.ReposManager.get_repos')
+    @patch('view.HomeView.HomeView.setRepos')
+    def test_new_repo_button_clicked(self, mock_repos_manager, mock_set_repos):
         app = QApplication([])
         homePage = Mock()
         homeModel =  Mock()
         homePage.repoList.currentText =  MagicMock(return_value='path')
+        mock_repos_manager.return_value = None
         homeController = HomeController()
-        homeController.repos_manager.get_repos = MagicMock(return_value=['path'])
         homeController.repos_manager.get_branches = MagicMock(return_value=['branch'])
         homeController._new_repo_button_clicked()
         self.assertIsNotNone(homeController.new_repo_controller)
 
     @patch('controller.TraceVisualizerController.TraceVisualizerController.__init__')
-    def test_search_button_clicked(self, mock_trace_visualizer_controller):
+    @patch('model.ReposManager.ReposManager.get_repos')
+    @patch('view.HomeView.HomeView.setRepos')
+    def test_search_button_clicked(self, mock_trace_visualizer_controller, mock_repos_manager, mock_set_repos):
         app = QApplication([])
-        homePage = Mock()
-        homeModel =  Mock()
         modification = Modification('commit','instruction', 'date', 'type', 'beforeCode', 'aftercode', 'hash', 'filename')
         modification2 = Modification('commit','instruction' ,'date', 'deleted', 'beforeCode', 'aftercode', 'hash', 'filename')
         logInstruction = LogInstruction('log.info("info")','instruction', [modification], '2023-01-01')
         logInstruction2 = LogInstruction('log.info("info")','instruction', [modification2], '2023-01-01')
-        homeModel.get_log_instructions = MagicMock(return_value= ([logInstruction], [logInstruction2]))
-        homePage.repoList.currentText =  MagicMock(return_value='path')
+        mock_repos_manager.return_value = None
+        mock_trace_visualizer_controller.return_value = None
+        mock_set_repos.return_value = None
         homeController = HomeController()
-        homeController.repos_manager.get_repos = MagicMock(return_value=['path'])
-        homeController.repos_manager.get_branches = MagicMock(return_value=['branch'])
         homeController.repos_manager.git_pull = Mock()
         homeController.home_view.from_calendar.selectedDate = MagicMock(return_value=QDate(2023, 1, 1))
         homeController.home_view.to_calendar.selectedDate = MagicMock(return_value=QDate(2023, 1, 2))
         captured_output = StringIO()
         sys.stdout = captured_output
-        mock_trace_visualizer_controller.return_value = None
         homeController._search_button_clicked()
         mock_trace_visualizer_controller.assert_called_once()
-
-    def test_delete_repo_button_clicked(self):
+    @patch('model.ReposManager.ReposManager.get_repos')
+    @patch('view.HomeView.HomeView.setRepos')
+    def test_delete_repo_button_clicked(self, mock_repos_manager, mock_set_repos):
         app = QApplication([])
         homePage = Mock()
+        mock_repos_manager.return_value = None
         homeController = HomeController()
         homeController.repos_manager.get_repos = MagicMock(return_value=['path'])
         homeController.home_view.setRepos = MagicMock(return_value=['path'])
