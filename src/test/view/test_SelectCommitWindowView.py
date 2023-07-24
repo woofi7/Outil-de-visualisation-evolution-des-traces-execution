7ac37e94b25980bdff30eab660a6df9ae3a70dfa
EduardoLozaRojas <73665126+EduardoLozaRojas@users.noreply.github.com>
2023-07-24 18:00:38 -0400

    ajout de test pour SelectCommitWindowView

---
+++ src/test/view/test_SelectCommitWindowView.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import Mock
+from view.SelectCommitWindowView import SelectCommitWindowView
+from PyQt6.QtWidgets import QApplication
+
+
+
+class test_SelectCommitWindowView(unittest.TestCase):
+        
+    
+  def test_CommitWindowView_Init(self):
+     app = QApplication([])
+     cwv = SelectCommitWindowView([['test','test','test','test'],'test','test','test'])
+     self.assertIsNotNone(cwv.code_tables)
+     self.assertIsNotNone(cwv.code_table)
+
+if __name__ == "__main__":
+    unittest.main()
