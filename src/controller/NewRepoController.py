3738e75da4924749554c6a6e0d5d0a07fea96acd
Nicolas Signori <n.signo@hotmail.com>
2023-11-06 14:41:51 -0500

    Add local repo support

--- src/controller/NewRepoController.py
+++ src/controller/NewRepoController.py
@@ -1,24 +1,30 @@
 import traceback
+
+from PyQt6.QtWidgets import QFileDialog
+
 from view.PopupView import PopupManager
 from model.ReposManager import ReposManager
 from view.NewRepoView import NewRepoView
+
 
 class NewRepoController:
     def __init__(self, home_view):
         self.new_repo_view = NewRepoView()
         self.home_view = home_view
         self.repo_manager = ReposManager()
-        self.new_repo_view.okButton.clicked.connect(self._ok_button_clicked)
+        self.new_repo_view.openButton.clicked.connect(self._open_file_dialog)
+
+        self.new_repo_view.cloneButton.clicked.connect(self._clone_repo)
         self.new_repo_view.cancelButton.clicked.connect(self._cancel_button_clicked)
 
-    def _ok_button_clicked(self):
+    def _clone_repo(self):
         try:
-            # Get the repository name and convert it to path
-            repo_name = self.new_repo_view.newRepo.text().split("/")[-1].split(".")[0]
+            # TODO: Handle clone and copy from view
+            repo_name = self.new_repo_view.cloneRepo.text().split("/")[-1].split(".")[0]
             repo_path = "./repo/" + repo_name + "/"
 
             # Clone the repository using the model
-            self.repo_manager.clone_repo(self.new_repo_view.newRepo.text(), repo_path)
+            self.repo_manager.clone_repo(self.new_repo_view.cloneRepo.text(), repo_path)
 
             # Close the current view
             self.new_repo_view.close()
@@ -30,5 +36,9 @@ class NewRepoController:
             PopupManager.show_info_popup("Caught Error", str(e))
 
     def _cancel_button_clicked(self):
-        #Close the current view
         self.new_repo_view.close()
+
+    def _open_file_dialog(self):
+        folder_path = QFileDialog.getExistingDirectory()
+        if folder_path:
+            self.new_repo_view.openRepo.setText(folder_path)
--- src/view/NewRepoView.py
+++ src/view/NewRepoView.py
@@ -1,30 +1,39 @@
-from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QGridLayout, QLineEdit
+from PyQt6.QtWidgets import QWidget, QPushButton, QLabel, QGridLayout, QLineEdit, QFrame, QFileDialog
 from view.PopupView import PopupManager
 import traceback
+
 
 class NewRepoView(QWidget):
     def __init__(self):
         try:
             super().__init__()
-            self.setWindowTitle("new repository")
+            self.setWindowTitle("New Repository")
             self.setGeometry(100, 100, 280, 80)
 
-            # Create a layout for the widget
             layout = QGridLayout(self)
 
-            #Create the search label
-            searchLabel = QLabel("Please enter the repository")
-            layout.addWidget(searchLabel)
+            # Clone remote
+            layout.addWidget(QLabel("Clone an external repository"))
+            self.cloneRepo = QLineEdit(self)
+            layout.addWidget(self.cloneRepo)
 
-            # Create allow the user to look for present repositories
-            self.newRepo = QLineEdit(self)
-            layout.addWidget(self.newRepo)
+            # Open local
+            layout.addWidget(QLabel("Open a local repository"))
+            self.openRepo = QLineEdit(self)
+            self.openButton = QPushButton("Select Folder")
+            layout.addWidget(self.openRepo)
+            layout.addWidget(self.openButton)
 
-            # Create the buttons for the repositories
-            self.okButton = QPushButton("ok")
-            self.cancelButton = QPushButton("cancel")
-            layout.addWidget(self.okButton)
+            line = QFrame()
+            line.setFrameShape(QFrame.Shape.HLine)  # This creates a horizontal line
+            line.setFrameShadow(QFrame.Shadow.Sunken)
+            layout.addWidget(line)
+
+            self.cloneButton = QPushButton("Clone")
+            layout.addWidget(self.cloneButton)
+            self.cancelButton = QPushButton("Cancel")
             layout.addWidget(self.cancelButton)
+
             self.show()
         except Exception as e:
             traceback.print_exc()
